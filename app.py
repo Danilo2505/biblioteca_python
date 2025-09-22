1e615259dd4df6fa2317d248b815248bf36c5b4b
Danilo25052 <josedanilobackups@gmail.com>
2025-09-22 11:49:46 -0300

    Implementação do Flask básico e reestruturação

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connect
+
+app = Flask(__name__)
+
+"""
+STATIC_FILE_DIRECTORY = "static"
+
+# Serve os arquivos estáticos
+@app.route("/static/<path:filename>")
+def serve_specific_file(filename):
+    return send_from_directory(STATIC_FILE_DIRECTORY, filename)
+"""
