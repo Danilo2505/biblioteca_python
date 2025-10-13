db728ceacb06c77bbe30f5b89bd6c1ff65895be8
Danilo2505 <77727610+Danilo2505@users.noreply.github.com>
2025-10-13 12:26:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-import mysql.connect
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -11,3 +11,24 @@ STATIC_FILE_DIRECTORY = "static"
 def serve_specific_file(filename):
     return send_from_directory(STATIC_FILE_DIRECTORY, filename)
 """
+
+def conectar():
+    conexao = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="aluno123",
+        database="biblioteca_python"
+    )
+    return conexao
+
+def adicionar_livro(titulo: str, autor: str, ano_publicacao: int):
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    sql = f"INSERT INTO livros(titulo, autor, ano_publicacao) VALUES (%s, %s, %s)"
+
+    valores = (titulo, autor, ano_publicacao)
+
+    cursor.excecute(sql, valores)
+
+    print("Livro Adicionado com Sucesso!!!")
