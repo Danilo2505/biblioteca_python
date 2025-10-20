a4d90762021a2a0df75e050b4a0f788784c725a0
Danilo2505 <77727610+Danilo2505@users.noreply.github.com>
2025-10-20 11:54:17 -0300

    UI de Terminal

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
+import os
 
 app = Flask(__name__)
 
@@ -11,6 +12,49 @@ STATIC_FILE_DIRECTORY = "static"
 def serve_specific_file(filename):
     return send_from_directory(STATIC_FILE_DIRECTORY, filename)
 """
+
+# ----- Auxiliares -----
+
+
+def limpar_terminal(aguardar: bool = False):
+    """
+    Limpa o terminal.
+    Se aguardar=True, o terminal só limpa depois do usuário pressionar ENTER.
+    """
+
+    if aguardar:
+        input("\nPressione ENTER para continuar...")
+
+    # Windows usa "cls", Linux/mac usa "clear"
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+# ----- Banco de Dados -----
+
+
+def inicializar_banco_de_dados():
+    # 1) Conecta sem o parâmetro "database"
+    conexao = mysql.connector.connect(host="localhost", user="root", password="")
+    cursor = conexao.cursor()
+
+    # 2) Cria o banco de dados se não existir
+    cursor.execute("CREATE DATABASE IF NOT EXISTS biblioteca_python")
+    cursor.execute("USE biblioteca_python")
+
+    # 3) Cria a tabela se não existir
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS livros (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            titulo VARCHAR(100) NOT NULL,
+            autor VARCHAR(100) NOT NULL,
+            ano_publicacao INT
+        )
+    """
+    )
+
+    conexao.commit()
+    conexao.close()
 
 
 def conectar():
@@ -58,7 +102,7 @@ def listar_livros():
             print(f"- Título: {livro[1]}")
             print(f"- Autor: {livro[2]}")
             print(f"- Ano de Publicação: {livro[3]}")
-            print(f"")
+            print("")
     else:
         print("Nenhum livro encontrado!")
 
@@ -66,13 +110,82 @@ def listar_livros():
 
 
 # --- Atualizar ---
-def atualizar_livro():
-    return
+def atualizar_livros(id_livro, novo_titulo, novo_autor, novo_ano):
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    sql = """
+    UPDATE livros
+    SET titulo = %s, autor = %s, ano_publicacao = %s
+    WHERE id = %s
+    """
+    valores = (novo_titulo, novo_autor, novo_ano, id_livro)
+
+    cursor.execute(sql, valores)
+    conexao.commit()
+    conexao.close()
 
 
 # --- Excluir ---
-def excluir_livro():
-    return
+def excluir_livro(id_livro):
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("DELETE FROM livros WHERE id = %s", (id_livro,))
+    conexao.commit()
+    conexao.close()
+
+    print("Livro Excluído com Sucesso!!!")
+
+
+def menu():
+    limpar_terminal(aguardar=False)
+    while True:
+        print("--- Sistema Biblioteca ---")
+        print("1 - Adicionar")
+        print("2 - Listar Livros")
+        print("3 - Atualizar Livros")
+        print("4 - Excluir Livros")
+        print("0 - Sair")
+        print("")
+
+        opcao = str(input("Escolha uma opção: "))
+
+        match opcao:
+            # Adicionar
+            case "1":
+                titulo = input("Título: ")
+                autor = input("Autor: ")
+                ano_publicacao = int(input("Ano de Publicação: "))
+
+                adicionar_livro(titulo, autor, ano_publicacao)
+            # Listar Livros
+            case "2":
+                listar_livros()
+            # Atualizar Livros
+            case "3":
+                listar_livros()
+
+                id_livro = int(input("ID do Livro: "))
+                novo_titulo = input("Novo Título: ")
+                novo_autor = input("Novo Autor: ")
+                novo_ano = int(input("Novo Ano de Publicação: "))
+
+                atualizar_livros(id_livro, novo_titulo, novo_autor, novo_ano)
+            # Excluir Livros
+            case "4":
+                listar_livros()
+
+                id_livro = int(input("ID do Livro: "))
+
+                excluir_livro(id_livro)
+            # Sair
+            case "0":
+                limpar_terminal(aguardar=False)
+                return 0
+            case _:
+                opcao = str(input("Escolha uma opção:"))
+        limpar_terminal(aguardar=True)
 
 
 def popular_db():
@@ -118,4 +231,6 @@ def popular_db():
 
 
 if __name__ == "__main__":
+    inicializar_banco_de_dados()
     popular_db()
+    menu()
--- schema.sql
+++ schema.sql
@@ -1,19 +1,14 @@
 # Cria o banco de dados biblioteca
-## CREATE database biblioteca_python;
+CREATE database biblioteca_python;
 
 # Define o banco de dados biblioteca como o
 # banco de dados padrão para a sessão atual
-## USE biblioteca_python;
+USE biblioteca_python;
 
 # Cria as tabelas
-/*
 CREATE table livros(
 	id INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(100) NOT NULL,
     autor VARCHAR(100) NOT NULL,
     ano_publicacao INT
 );
-*/
-
-# Seleciona todos os livros
-SELECT * FROM livros;
